3213b4ef276a1356858746610823293dbd4a2655
Rambabu <rambabu@example.com>
2022-10-31 09:15:59 +0000

    superOverLeague

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="Container">
+    <h1 className="greeting">Super Over League</h1>
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      className="image"
+    />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      className="image"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
